44c230f986a5957eed6c15729fef19e6f7a299eb
bagman-0 <d_glutamine@protonmail.com>
2023-07-09 00:06:07 -0400

    still testing cronjob. added pkill command

--- cogs/feet.py
+++ cogs/feet.py
@@ -84,7 +84,7 @@ class Foot(commands.Cog, name="foot"):
         if message.author == self.bot.user or message.author.bot:
             return
         if message.content.find('pig'.lower()) != -1:
-            await message.channel.send('piggs')
+            await message.channel.send('piglets')
 
 # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
 async def setup(bot):
